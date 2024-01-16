63ed87804740038fb9d4064a038f1b49eb436222
Jeremy Fairbanks <jsfairbanks@gmail.com>
2024-01-16 11:30:25 -0500

    Rearranged layout order

--- rice/layouts.lua
+++ rice/layouts.lua
@@ -8,11 +8,11 @@ local tilted = require("layouts.tilted")
 ---@field list awful.layout[]
 local layouts = {
     list = core.layout.initialize_list {
-        tilted.new("tiling"),
-        awful.layout.suit.tile.left,
-        awful.layout.suit.corner.nw,
-        awful.layout.suit.corner.ne,
         awful.layout.suit.fair,
+        awful.layout.suit.corner.ne,
+        awful.layout.suit.tile.left,
+        -- awful.layout.suit.corner.nw,
+        tilted.new("tiling"),
         awful.layout.suit.magnifier,
         -- awful.layout.suit.floating,
         awful.layout.suit.max,
