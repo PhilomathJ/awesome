778c7a6f4cd5d7738f309969c01b1ec9bf7d6ba0
Jeremy Fairbanks <jsfairbanks@gmail.com>
2024-03-06 11:28:07 -0500

    Autorun changes

--- autorun.lua
+++ autorun.lua
@@ -1,7 +1,12 @@
 -- requirements
 -- ~~~~~~~~~~~~
+-- local capi = {
+--     screen = screen,
+--     mouse = mouse,
+--     client = client
+-- }
 local awful = require "awful"
-local gfs = require "gears".filesystem.get_configuration_dir()
+local capi = Capi
 
 -- autostart
 -- ~~~~~~~~~
@@ -19,11 +24,11 @@ end
 
 
 local applications = {
-    "nitrogen --restore", -- wallpaper
+    -- "nitrogen --restore", -- wallpaper
     "touchegg", -- Trackpad gestures,
-    "signal-desktop",
-    "/opt/brave-bin/brave --profile-directory=Default --app-id=hpfldicfbfomlpcikngkocigghgafkph", -- Google Messages PWA
-    "/usr/bin/Discord",
+    -- "signal-desktop",
+    -- "/opt/brave-bin/brave --profile-directory=Default --app-id=hpfldicfbfomlpcikngkocigghgafkph", -- Google Messages PWA
+    -- "/usr/bin/Discord",
     -- "/opt/brave-bin/brave --profile-directory=Default --app-id=agimnkijcaahngcdmfeangaknmldooml" -- YouTube PWA
     -- "feh --no-fehbg --bg-fill ~/Immagini/Arch/Arch-abstract2.jpg --bg-fill ~/Immagini/Arch/Arch-abstract1.jpg",
     -- "picom --config $HOME/.config/awesome/misc/picom/panthom.conf &",
@@ -35,3 +40,30 @@ local applications = {
 for _, prc in ipairs(applications) do
     run(prc)
 end
+-------------------------------------------------------------------------
+-- only-one-time applications
+local function run_once(command)
+    local is_running = false   -- flag is an instance is already running
+
+    for _, c in ipairs(capi.client.get()) do
+        if c.instance == "crx_hpfldicfbfomlpcikngkocigghgafkph" then
+            is_running = true
+        end
+    end
+
+    if is_running then return end -- TODO: apply focus
+
+    awful.spawn.easy_async_with_shell(command)
+end
+
+local one_time_applications = {
+    -- "dex $HOME/.local/share/brave-hpfldicfbfomlpcikngkocigghgafkph.desktop",
+    -- "/opt/brave-bin/brave --profile-directory=Default --app-id=agimnkijcaahngcdmfeangaknmldooml" -- YouTube PWA
+    "/opt/brave-bin/brave  --profile-directory=Default --app-id=hpfldicfbfomlpcikngkocigghgafkph" -- Google Messages
+}
+
+-- for _, prc in ipairs(one_time_applications) do
+--     run_once(prc)
+-- end
+
+-- awful.spawn.with_shell("~/.config/awesome/scripts/autorun.sh")
